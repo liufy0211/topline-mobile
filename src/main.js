ddecd288b5f1dbdeb30a90f59e5255be8fe89814
liufy <liufy0211@163.com>
2019-07-09 22:53:17 +0800

    登录-页面布局

--- src/main.js
+++ src/main.js
@@ -6,12 +6,22 @@ import store from './store'
   这个第三方包引入即可，它会自动根据页面宽度设置网页基准字体大小
 */
 import 'amfe-flexible'
-// 移动端有好处 会减少代码打包体积
-import {
-  Button
-} from 'vant'
+/**
+ * 现在我们简单粗暴全引进来，方便使用
+ */
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
-Vue.use(Button)
+Vue.use(Vant)
+/*
+  移动端有好处 会减少代码打包体积
+  后面我们再配置为按需引入的方式（有利于打包结果优化）
+*/
+// import {
+//   Button
+// } from 'vant'
+
+// Vue.use(Button)
 
 Vue.config.productionTip = false
 
