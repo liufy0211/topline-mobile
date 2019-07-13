e01f2786520cd36e4f03d1c990b6339c4979ee80
liufy <liufy0211@163.com>
2019-07-13 20:06:15 +0800

    处理路由缓存后登陆重新获取用户频道数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -81,6 +81,19 @@ export default {
       return this.channels[this.activeChannelIndex]
     }
   },
+
+  watch: {
+    // 监视容器中的user用户
+    // 记住：凡是能this. 点儿出来的成员度可以直接在这里监视
+    // 由于路由缓存了，所以这里监视用户的登录状态，如果登录了。则加载用户的频道列表
+    async '$store.state.user' () {
+      // 重新加载用户频道列表
+      this.loadChannels()
+      // 频道数据改变，重新加载当前激活频道的数据
+      // 只需将上拉加载更多设置为 true，它就会自动去调用 onLoad 请求函数
+      this.activeChannel.upPullLoading = true
+    }
+  },
   created () {
     this.loadChannels()
   },
