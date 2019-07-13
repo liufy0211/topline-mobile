0a9a05018001c6e2ec3c489813409f63e01f7fb1
liufy <liufy0211@163.com>
2019-07-13 15:09:35 +0800

    首页文章列表-上拉加载更多

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -23,8 +23,8 @@
           onLoad 事件会在滚动的底部区域的时候自动调用,每次onLoad它会自动让loading 为true
          -->
         <van-list
-          v-model="loading"
-          :finished="finished"
+          v-model="channelItem.upPullLoading"
+          :finished="channelItem.upPullFinished"
           finished-text="没有更多了"
           @load="onLoad"
         >
@@ -32,9 +32,9 @@
           列表中的内容
          -->
           <van-cell
-            v-for="item in list"
-            :key="item"
-            :title="item"
+            v-for="articleItem in channelItem.articles"
+            :key="articleItem.art_id"
+            :title="articleItem.title"
           />
         </van-list>
         </van-pull-refresh>
@@ -55,6 +55,7 @@
 <script>
 import { setTimeout } from 'timers'
 import { getUserChannels } from '@/api/channel'
+import { getArticles } from '@/api/article'
 export default {
   name: 'HomeIndex',
   data () {
@@ -65,6 +66,14 @@ export default {
       finished: false,
       pullRefreshLoading: false,
       channels: [] // 存储频道列表
+    }
+  },
+  computed: {
+    /*
+      当前激活的频道
+    */
+    activeChannel () {
+      return this.channels[this.activeChannelIndex]
     }
   },
   created () {
@@ -94,6 +103,7 @@ export default {
       // 修改channels 将这个数据结构修改为满足我们使用的需求
       channels.forEach(item => {
         item.articles = [] // 用来存储当前文章的列表 item 是一个一个的频道对象
+        item.timestamp = Date.now() // 存储下一页数据的时间戳
         item.downPullLoading = false // 控制当前频道的下拉刷新 loading 状态
         item.upPullLoading = false // 控制当前频道的上拉加载等多的 loading状态
         item.upPullFinished = false // 控制当前频道数据是否加载完毕
@@ -101,8 +111,29 @@ export default {
       this.channels = channels
     },
     // 上拉加载更多，push 数据
-    onLoad () {
+    async onLoad () {
       console.log('onLoad') // 一上来onload 好几次 ，当发现占不满一屏的时候，会再去onload 它自动的
+      let data = []
+      data = await this.loadArticles()
+      // pre_timestamp 下一页的页码 （下一页的数据，也就是上次时间点推荐的数据）每一次会告诉你上一次推荐数据标志 上一次最新的数据在pre_timestamp
+      // results 文章列表
+      // console.log(data)
+
+      // 解决初始化的时候没有最新推荐数据的问题 没有最新数据，那就加载上一次推荐数据
+      if (data.pre_timestamp && !data.results.length) {
+        this.activeChannel.timestamp = data.pre_timestamp
+        // 加载下一页数据
+        data = await this.loadArticles()
+      }
+
+      // 数据加载好以后，将 pre_timestamp 更新到当前频道中 用来加载下下页数据
+      this.activeChannel.timestamp = data.pre_timestamp
+      // 将文章数据更新到频道中(注意：是push追加 不是覆盖)
+      this.activeChannel.articles.push(...data.results)
+      // 数据加载完毕，取消loading
+      this.activeChannel.upPullLoading = false
+      // console.log(data)
+
       // 异步更新数据  每onLoad一下 就要加载数据 往list里面 push 数据
       setTimeout(() => {
         for (let i = 0; i < 10; i++) {
@@ -124,6 +155,15 @@ export default {
       setTimeout(() => {
         this.pullRefreshLoading = false
       }, 3000)
+    },
+    async loadArticles () {
+      const { id: channelId, timestamp } = this.activeChannel
+      const data = await getArticles({
+        channelId, // 当前激活频道id
+        timestamp, // 默认当前最新时间 下一页数据的时间戳 // Date.now()获取当前时间戳 用于请求分页数据的事件戳
+        withTop: 1 // 是否包含置顶数据
+      })
+      return data
     }
   }
 }
