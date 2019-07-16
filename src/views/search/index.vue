7cd7764e6269521c017dcd26f7991edfb7a1357a
liufy <liufy0211@163.com>
2019-07-17 02:54:26 +0800

    搜索-联想建议-关键字高亮

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,8 +11,13 @@
         icon="search"
         v-for="item in suggestions"
         :key="item"
-        :title="item"
-      />
+      >
+      <!-- {{}} 无法输出html字符内容 -->
+      <!-- v-html 指令才会解析字符串中的html -->
+      <!-- <div slot="title">{{ item }}</div> -->
+      <!-- 过滤器只能用在{{}} 和v-bind 中 -->
+      <div slot="title" v-html="hightlight(item, searchText)"></div>
+      </van-cell>
     </van-cell-group>
     <!-- /联想建议列表 -->
 
@@ -32,7 +37,8 @@
 
 <script>
 import { getSuggestion } from '@/api/search'
-import { debounce } from 'lodash'
+import { debounce } from 'lodash' // _. 里面有什么成员，我们就声明什么成员
+// import _ from 'lodash' 全部加载进来 然后再_.debounce 它里面又很多函数，我们建议按需加载
 export default {
   name: 'SearchIndex',
   data () {
@@ -42,11 +48,14 @@ export default {
     }
   },
   // 监视它 当它改变就发请求 newVal 当前最新值 oldVal 变化之前的值
+
+  // watch 里 searchText: 这个名字不能随便起 函数名就是你需要监视的数据成员
   watch: {
     // debounce 接收两个参数
     // 第一个参数： 执行你的业务逻辑的参数函数
     // 第二个参数： 防抖时间
     // 当你同一时间调用频率过快，只有挺下来经过指定的时间才会来调用
+    // 它每一次都会来调 只是调的时候不够500ms，在500ms以内又触发了这个函数，它就再次延误500ms，直到停下来超过500ms才真的来调
     searchText: debounce(async function (newVal) {
       newVal = newVal.trim() // 去除首位空格
 
@@ -59,6 +68,12 @@ export default {
       // console.log(data)
       this.suggestions = data.options
     }, 500)
+  },
+  methods: {
+    hightlight (text, keyword) {
+      return text.toLowerCase().split(keyword)
+        .join(`<span style="color: red">${keyword}</span>`)
+    }
   }
 }
 </script>
