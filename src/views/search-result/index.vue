3760316bf76b8f3eda004565c835dc2af65c0b57
liufy <liufy0211@163.com>
2019-07-18 02:39:59 +0800

    搜索结果-列表展示

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -6,6 +6,7 @@
       left-text="返回"
       left-arrow
       fixed
+      @click-left="$router.back()"
     />
     <!-- 顶部导航 -->
 
@@ -18,9 +19,9 @@
       @load="onLoad"
     >
       <van-cell
-        v-for="item in list"
-        :key="item"
-        :title="item"
+        v-for="item in articles"
+        :key="item.art_id.toString()"
+        :title="item.title"
       />
     </van-list>
     <!-- /文章列表 -->
@@ -35,7 +36,16 @@ export default {
     return {
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1,
+      perpage: 10,
+      articles: []
+    }
+  },
+
+  computed: {
+    q () {
+      return this.$route.params.q
     }
   },
 
@@ -47,19 +57,43 @@ export default {
   },
 
   methods: {
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
+    async onLoad () {
+      // // 异步更新数据
+      // setTimeout(() => {
+      //   for (let i = 0; i < 10; i++) {
+      //     this.list.push(this.list.length + 1)
+      //   }
+      //   // 加载状态结束
+      //   this.loading = false
+      //   // 数据全部加载完成
+      //   if (this.list.length >= 40) {
+      //     this.finished = true
+      //   }
+      // }, 500)
+      const data = await this.getSearchResults()
+      // 如果请求结果数组为空，则设置List 组件已加载结束
+      if (!data.results.length) {
         this.loading = false
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+        this.finished = true
+        return
+      }
+
+      // 如果由数据，则将本次加载到的数据 push 到列表数组中
+      this.articles.push(...data.results)
+      // 数据加载完毕，更新当前页码为下一页，用于下一次加载更多
+      this.page += 1
+      // 结束当前加载的loading
+      // List 列表组件每次onLoad 会自动将 loading 设置为true
+      // 如果你不设置的话，它不会触发下一次的onLoad
+      this.loading = false
+    },
+    getSearchResults () {
+      // 每次滚动加载 computed里的this.$route.params.q 数据没有变 直接绑定使用this.q了
+      return getSearch({
+        page: this.page,
+        perpage: this.perpage,
+        q: this.q
+      })
     }
   }
 }
