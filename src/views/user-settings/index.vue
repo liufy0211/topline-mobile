8ef185d66b864692440e9fd617f5b0c78cb84aab
liufy <liufy0211@163.com>
2019-07-22 22:21:36 +0800

    编辑个人信息-数据展示

--- src/views/user-settings/index.vue
+++ src/views/user-settings/index.vue
@@ -9,22 +9,42 @@
       @click-right="handleSave"
     />
     <van-cell-group>
-      <van-cell title="头像" value="内容" />
-      <van-cell title="昵称" value="内容" />
-      <van-cell title="介绍" value="内容" />
-      <van-cell title="性别" value="内容" />
-      <van-cell title="生日" value="内容" />
+      <!-- 头像展示图片 value 默认只能显示文本 -->
+      <van-cell title="头像" is-link>
+        <div slot="default">
+          <!-- default 自定义value 显示内容 -->
+          <img width="30" :src="user.photo" alt="">
+        </div>
+        <!-- <div slot="title"></div> 插槽渲染到title里 默认情况下只能写文本 但是在div里可以写标签 写什么都行 会渲染到标题哪里-->
+      </van-cell>
+      <van-cell title="昵称" :value="user.name" is-link />
+      <van-cell title="性别" :value="user.gender" is-link />
+      <van-cell title="生日" :value="user.birthday" is-link />
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
 export default {
   name: 'UserSettings',
   data () {
-    return {}
+    return {
+      user: {}
+    }
+  },
+
+  created () {
+    this.loadUser()
   },
   methods: {
+    async loadUser () {
+      try {
+        this.user = await getUserProfile()
+      } catch (err) {
+        this.$toast.fail('加载用户信息失败')
+      }
+    },
     handleSave () {
     }
   }
