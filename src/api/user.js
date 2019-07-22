3545df577bb689a92954629dce6c1f99618accfd
liufy <liufy0211@163.com>
2019-07-22 20:23:09 +0800

    我的模块-展示用户登录信息

--- src/api/user.js
+++ src/api/user.js
@@ -72,3 +72,13 @@ export const unFollowUser = userId => {
     url: `/app/v1_0/user/followings/${userId}`
   })
 }
+
+/*
+  获取用户自己信息
+*/
+export const getMyInfo = userId => {
+  return request({
+    method: 'GET',
+    url: `/app/v1_0/user`
+  })
+}
